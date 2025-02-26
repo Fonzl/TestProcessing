88b39d1eedad83adb4ce4466d384c4065cd9ce7b
hrus <hrustaI@mail.ru>
2025-02-26 20:52:30 +0300

    НачалРелизациюОтслеживанияТеста

---
+++ Repository/Migrations/20250226141743_UpdateDataClassUserResponses.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDataClassUserResponses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFinish",
+                table: "UserResponses",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "dateTime",
+                table: "UserResponses",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFinish",
+                table: "UserResponses");
+
+            migrationBuilder.DropColumn(
+                name: "dateTime",
+                table: "UserResponses");
+        }
+    }
+}
--- Repository/RepositoryResultTest/IRepositoryResultTest.cs
+++ Repository/RepositoryResultTest/IRepositoryResultTest.cs
@@ -16,6 +16,7 @@ namespace Repository.RepositoryResultTest
         void Update(UpdateResultTestDto dto);
         void Delete(int id);
         ResultOfAttemptsDTO InsertStudent(AddResultTestStudentDto dto);
+        ResultOfAttemptsDTO InsertStudent(long testId,long studentId);
         List<VerifiedUserResponesDto> returnResultDetails(long idResulTest);
     }
 }
